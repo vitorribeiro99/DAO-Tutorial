d993c5e6f8c78f5f791e7f0ccff78794fbf6fb6e
Vitor Ribeiro <v99ribeiro@gmail.com>
2023-03-14 09:31:01 +0000

    initial commit

---
+++ backend/constants.js
@@ -0,0 +1,5 @@
+// Replace the value with your NFT contract address
+const CRYPTODEVS_NFT_CONTRACT_ADDRESS =
+  "0x277E9526e6ccA607b13b39e3Dd3CD9B2aAF5DbB8";
+
+module.exports = { CRYPTODEVS_NFT_CONTRACT_ADDRESS };
